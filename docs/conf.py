8abc98b293fe922dbacb9e1a4ff84aa2692ad0df
Miguel <38389409+m1guelvrrl0@users.noreply.github.com>
2025-10-05 15:30:07 +0700

    Fix Read the Docs build configuration
    - Remove package installation from .readthedocs.yaml to avoid setuptools/hatchling conflict
    - Add path configuration to docs/conf.py for local imports
    - Mock asyncpg imports for documentation build
    - Update MANIFEST.in to include WebP images
    - Add PostgreSQL meme to README

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -58,6 +62,9 @@ autodoc_default_options = {
     'undoc-members': True,
     'exclude-members': '__weakref__'
 }
+
+# Mock imports for packages not available during documentation build
+autodoc_mock_imports = ['asyncpg']
 
 # Intersphinx mapping
 intersphinx_mapping = {
